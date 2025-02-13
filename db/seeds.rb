e174d277b2062afa3dff630a201b921c800cca6a
kxusx <khush99199@gmail.com>
2025-02-12 18:24:14 -0600

    updated seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,19 @@
 #   end
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighborj Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Paraasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  {:title => 'My Neighbor Totoro', :rating => 'G', :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13', :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R', :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R', :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13', :release_date => '13-Aug-2021'},
+  {:title => 'The Power of the Dog', :rating => 'R', :release_date => '26-Nov-2021'},
+  {:title => 'Pulp Fiction', :rating => 'R', :release_date => '19-Oct-1994'},
+  {:title => 'The Hateful Eight', :rating => 'R', :release_date => '9-Jan-2016'},
+  {:title => 'Once Upon a Time in Hollywood', :rating => 'R', :release_date => '27-Jul-2019'},
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by!(title: movie[:title], release_date: movie[:release_date]) do |m|
+    m.rating = movie[:rating]
+  end
 end
